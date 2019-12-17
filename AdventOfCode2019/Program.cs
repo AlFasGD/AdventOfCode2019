02a6b88d1bfb140858868db9d204e57c1b3754ad
AlFas <AlFasGD@users.noreply.github.com>
2019-12-17 17:05:52 +0200

    Solve day 14

--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode2019
     {
         public static void Main(string[] args)
         {
-            RunProblem(new Day13());
+            RunProblem(new Day14());
         }
 
         private static void RunProblem<T1, T2>(Problem<T1, T2> instance)
---
+++ AdventOfCode2019/Utilities/IKeyedObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2019.Utilities
+{
+    public interface IKeyedObject<T>
+    {
+        T Key { get; }
+    }
+}
